c1b5e25542ecb7d3d93fb6c86005ded5d6dafd67
Whilfred <fathanemarcos169@gmail.com>
2026-01-13 14:13:24 +0000

    improve design video

--- front_end/app/(tabs)/ShopScreen.tsx
+++ front_end/app/(tabs)/ShopScreen.tsx
@@ -28,7 +28,7 @@ interface Category {
 
 interface ShopProduct extends BaseProductType {}
 
-const API_BASE_URL = "http://192.168.11.107:3001/api";
+const API_BASE_URL = "http://192.168.11.114:3001/api";
 
 export default function TabShopScreen() {
   const router = useRouter();
--- front_end/app/OrdersScreen.tsx
+++ front_end/app/OrdersScreen.tsx
@@ -31,7 +31,7 @@ interface Order {
   products: OrderItem[];
 }
 
-const API_BASE_URL = "http://192.168.11.107:3001/api";
+const API_BASE_URL = "http://192.168.11.114:3001/api";
 
 export default function OrdersScreen() {
   const { userToken, effectiveAppColorScheme } = useAuth();
